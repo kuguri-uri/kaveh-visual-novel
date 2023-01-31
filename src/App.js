cd2c1f756046153f1f0be9a1c9fa6ce818087871
Vera <mokeeva.vk@phystech.edu>
2023-01-31 21:13:32 +0300

    bruh its somehow working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useState } from 'react';
+
 import dude from './resources/img/dude.png';
-import face from './resources/img/dude_face.png'
+// import face from './resources/img/dude_face.png'
 import './App.css';
 
+
+
 function App() {
+  const [data, setData] = useState({
+    slide: 0,
+    title: 'Kaveh',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quas nam pariatur eaque provident ab eius necessitatibus dolore aspernatur explicabo odio ullam, obcaecati ipsa nesciunt quisquam. Non voluptatem pariatur commodi!'
+  })
+
+  // const changeData = (data) => {
+  //   switch (data.slide) {
+  //     case 1:
+  //       console.log('slide 1');
+  //       setData({
+  //         title: 'Kaveh',
+  //         text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus quas nam pariatur eaque provident ab eius necessitatibus dolore aspernatur explicabo odio ullam, obcaecati ipsa nesciunt quisquam. Non voluptatem pariatur commodi!'  
+  //       });
+  //       break;
+  //     case 2:
+  //       console.log('slide 2');
+  //       setData({
+  //         title: 'Alhaitham',
+  //         text: 'Kaveh shut up'  
+  //       });
+  //       break;
+  //     default:
+  //       console.log(`dude wtf`);
+  //   }
+  // }
+
+  const titleArr = ["Title1", "Title2", "Title3", "Title4"];
+  const textArr = ["Text1", "Text2", "Text3", "Text4"];
+
+
   return (
     <div className="App">
       <div className="buttons"></div>
@@ -14,10 +49,30 @@ function App() {
       </div>
 
       <div className="textarea">
-        <div className="title">Kaveh</div>
+        <div className="title">{data.title}</div>
         <span></span>
-        <div className="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi repudiandae vero consequuntur quo nesciunt exercitationem corporis, repellendus consectetur repellat quia suscipit molestias aliquam nemo. Eaque ad accusantium quae quidem sequi!</div>
+        <div className="text">{data.text}</div>
       </div>
+
+      <button 
+        className="Next"
+        onClick={() => setData({
+          slide: data.slide + 1,
+          title: `${titleArr[data.slide]}`,
+          text: `${textArr[data.slide]}`
+        })}>
+        Next
+      </button>
+
+      <button 
+        className="Prev"
+        onClick={() => setData({
+          slide: data.slide - 1,
+          title: `${titleArr[data.slide]}`,
+          text: `${textArr[data.slide]}`
+        })}>
+        Prev
+      </button>
     </div>
   );
 }
